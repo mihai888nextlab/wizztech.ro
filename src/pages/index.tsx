6f88eb90a5a2013b39316ca370648959a25d7a9f
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2024-12-13 15:24:20 +0200

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <main>
-        <div className="bg-slate-200">
-
-        </div>
-      </main>
-    </div>  
+    </div>
   );
 }
